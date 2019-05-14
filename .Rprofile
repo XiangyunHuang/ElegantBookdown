4b72b0fcb16e8cc4fb74468599c848c8875d6de4
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-05-14 22:00:30 +0800

    跑通模版

---
+++ .Rprofile
@@ -0,0 +1,17 @@
+#options(bookdown.post.latex = function(x) {
+  # https://pandoc.org/MANUAL.html#columns
+  # x <- gsub("\\begin{columns}[T]", "", x, fixed = TRUE)
+  # x <- gsub("\\end{columns}", "", x, fixed = TRUE)
+  # 
+  # i2 <- which(x == "\\end{column}")
+  # 
+  # i <- seq(length(i2) / 2)
+  # x <- x[-(i2[2 * i - 1] + 1)] # delete newline
+  # 
+  # x <- gsub(x,
+  #           pattern = "(\\\\begin\\{)column(\\}\\{)(0.\\d+)(\\\\textwidth\\})",
+  #           replacement = "\\1minipage\\2\\3\\4", fixed = FALSE
+  # )
+  # x <- gsub("\\end{column}", "\\end{minipage}", x, fixed = TRUE)
+  # x
+#})
--- latex/sidebar.lua
+++ latex/sidebar.lua
@@ -7,109 +7,14 @@ function Span(el)
       pandoc.RawInline("latex", "}")
     }
   end
-
-  if el.classes:includes("main") then
-    return {
-      pandoc.RawInline("latex", "\\textcolor{main}{"),
-      el,
-      pandoc.RawInline("latex", "}")
-    }
-  end
-
-  if el.classes:includes("second") then
-    return {
-      pandoc.RawInline("latex", "\\textcolor{second}{"),
-      el,
-      pandoc.RawInline("latex", "}")
-    }
-  end
-
-  if el.classes:includes("third") then
-    return {
-      pandoc.RawInline("latex", "\\textcolor{third}{"),
-      el,
-      pandoc.RawInline("latex", "}")
-    }
-  end
 end
 
 function Div(el)
   if el.classes:includes("sidebar") then
     return {
-      pandoc.RawBlock("latex", "\\begin{kframe}"),
+      pandoc.RawBlock("latex", "\\begin{shaded}"),
       el,
-      pandoc.RawBlock("latex", "\\end{kframe}")
-    }
-  end
-
-  if el.classes:includes("note") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{note}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{note}")
-    }
-  end
-
-  if el.classes:includes("definition") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{definition}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{definition}")
-    }
-  end
-  if el.classes:includes("remark") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{remark}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{remark}")
-    }
-  end
-
-  if el.classes:includes("introduction") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{introduction}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{introduction}")
-    }
-  end
-
-  if el.classes:includes("conclusion") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{conclusion}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{conclusion}")
-    }
-  end
-
-  if el.classes:includes("example") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{example}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{example}")
-    }
-  end
-
-  if el.classes:includes("solution") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{solution}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{solution}")
-    }
-  end
-
-  if el.classes:includes("problem") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{problem}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{problem}")
-    }
-  end
-
-  if el.classes:includes("warning") then
-    return {
-      pandoc.RawBlock("latex", "\\begin{kframe} \\textbf{警告}"),
-      el,
-      pandoc.RawBlock("latex", "\\end{kframe}")
+      pandoc.RawBlock("latex", "\\end{shaded}")
     }
   end
 end
