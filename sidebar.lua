634660a478acfa9845d8762822a2ed1fa7d927b1
xiangyun huang <xiangyunfaith@outlook.com>
2020-03-28 15:34:14 +0800

    don't let rmdwarn, rmdnote and rmdtip blocks indent

--- sidebar.lua
+++ sidebar.lua
@@ -19,21 +19,21 @@ function Div(el)
   end
   if el.classes:includes("rmdwarn") then
     return {
-      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor[RGB]{234,67,53}{\\textbf{警告}}"),
+      pandoc.RawBlock("latex", "\\begin{shaded}\\par\\noindent\\textcolor[RGB]{234,67,53}{\\textbf{警告}}"),
       el,
       pandoc.RawBlock("latex", "\\end{shaded}")
     }
   end
   if el.classes:includes("rmdnote") then
     return {
-      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor[RGB]{251,188,5}{\\textbf{注意}}"),
+      pandoc.RawBlock("latex", "\\begin{shaded}\\par\\noindent\\textcolor[RGB]{251,188,5}{\\textbf{注意}}"),
       el,
       pandoc.RawBlock("latex", "\\end{shaded}")
     }
   end
   if el.classes:includes("rmdtip") then
     return {
-      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor[RGB]{52,168,83}{\\textbf{提示}}"),
+      pandoc.RawBlock("latex", "\\begin{shaded}\\par\\noindent\\textcolor[RGB]{52,168,83}{\\textbf{提示}}"),
       el,
       pandoc.RawBlock("latex", "\\end{shaded}")
     }
