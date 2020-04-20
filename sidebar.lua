a5891b3f4acde3f25f35c7acf636bf3a02980943
F Y <fyemath@hotmail.com>
2020-04-20 15:07:55 -0400

    using lua to create latex thm environments for elegantbook.

--- sidebar.lua
+++ sidebar.lua
@@ -39,3 +39,105 @@ function Div(el)
     }
   end
 end
+
+
+function Div(el)
+    local thmname=""
+    local thmlabel=""
+    if el.attributes.name then
+        thmname=el.attributes.name
+    end
+    if el.attributes.label then
+        thmlabel=el.attributes.label
+    end
+
+    if el.classes:includes("thm") then
+        return {
+            pandoc.RawBlock("latex", table.concat({"\\begin{theorem}", "{", thmname, "}", "{", thmlabel, "}"})),
+            el,
+            pandoc.RawBlock("latex", "\\end{theorem}")
+        }
+    end
+
+    if el.classes:includes("defn") then
+    return {
+        pandoc.RawBlock("latex", table.concat({"\\begin{definition}", "{", thmname, "}", "{", thmlabel, "}" })),
+        el,
+        pandoc.RawBlock("latex", "\\end{definition}")
+    }
+    end
+    if el.classes:includes("exam") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{example}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{example}")
+    }
+    end
+    if el.classes:includes("exer") then
+        return {
+            pandoc.RawBlock("latex", "\\begin{exercise}"),
+            el,
+            pandoc.RawBlock("latex", "\\end{exercise}")
+        }
+    end
+    if el.classes:includes("sol") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{solution}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{solution}")
+    }
+    end
+    if el.classes:includes("lem") then
+    return {
+        pandoc.RawBlock("latex", table.concat({"\\begin{lemma}", "{", thmname, "}", "{", thmlabel, "}" }) ),
+        el,
+        pandoc.RawBlock("latex", "\\end{lemma}")
+    }
+    end
+    if el.classes:includes("cor") then
+    return {
+        pandoc.RawBlock("latex", table.concat({"\\begin{corollary}", "{", thmname, "}", "{", thmlabel, "}" }) ),
+        el,
+        pandoc.RawBlock("latex", "\\end{corollary}")
+    }
+    end
+    if el.classes:includes("prop") then
+    return {
+        pandoc.RawBlock("latex", table.concat({"\\begin{proposition}", "{", thmname, "}", "{", thmlabel, "}" }) ),
+        el,
+        pandoc.RawBlock("latex", "\\end{proposition}")
+    }
+    end
+
+    if el.classes:includes("twocols") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{multicols}{2}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{multicols}")
+    }
+    end
+    if el.classes:includes("threecols") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{multicols}{3}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{multicols}")
+    }
+    end
+    if el.classes:includes("fourcols") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{multicols}{4}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{multicols}")
+    }
+    end
+end
+
+function Image (el)
+    if el.classes:includes("center") then
+    return {
+        pandoc.RawInline('latex', '\\hfill\\break{\\centering'),
+        el,
+        pandoc.RawInline('latex', '\\par}')
+    }
+    end
+end
