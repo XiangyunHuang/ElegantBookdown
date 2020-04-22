7abbeeb772cbc2700b41891a10a8a62ad5e090aa
F Y <fyemath@hotmail.com>
2020-04-21 23:02:30 -0400

    重排了css，定理类名称使用变量，容易中英文切换。
    添加了更多类的定理环境名称。引用的问题无法完美解决。可以额外添加一个id。但是不知道咋显示数字。

--- sidebar.lua
+++ sidebar.lua
@@ -50,65 +50,83 @@ function Div(el)
     if el.attributes.label then
         thmlabel=el.attributes.label
     end
-
-    if el.classes:includes("thm") then
+    if el.classes:includes("thm" or "rmdthm" or "rmdtheorem") then
         return {
             pandoc.RawBlock("latex", table.concat({"\\begin{theorem}", "{", thmname, "}", "{", thmlabel, "}"})),
             el,
             pandoc.RawBlock("latex", "\\end{theorem}")
         }
     end
-
-    if el.classes:includes("defn") then
-    return {
-        pandoc.RawBlock("latex", table.concat({"\\begin{definition}", "{", thmname, "}", "{", thmlabel, "}" })),
-        el,
-        pandoc.RawBlock("latex", "\\end{definition}")
-    }
-    end
-    if el.classes:includes("exam") then
-    return {
-        pandoc.RawBlock("latex", "\\begin{example}"),
-        el,
-        pandoc.RawBlock("latex", "\\end{example}")
-    }
-    end
-    if el.classes:includes("exer") then
-        return {
-            pandoc.RawBlock("latex", "\\begin{exercise}"),
-            el,
-            pandoc.RawBlock("latex", "\\end{exercise}")
-        }
-    end
-    if el.classes:includes("sol") then
-    return {
-        pandoc.RawBlock("latex", "\\begin{solution}"),
-        el,
-        pandoc.RawBlock("latex", "\\end{solution}")
-    }
-    end
-    if el.classes:includes("lem") then
+    if el.classes:includes("lem" or "rmdlem" or "rmdlemma") then
     return {
         pandoc.RawBlock("latex", table.concat({"\\begin{lemma}", "{", thmname, "}", "{", thmlabel, "}" }) ),
         el,
         pandoc.RawBlock("latex", "\\end{lemma}")
     }
     end
-    if el.classes:includes("cor") then
+    if el.classes:includes("cor" or "rmdcor" or "rmdcorollary") then
     return {
         pandoc.RawBlock("latex", table.concat({"\\begin{corollary}", "{", thmname, "}", "{", thmlabel, "}" }) ),
         el,
         pandoc.RawBlock("latex", "\\end{corollary}")
     }
     end
-    if el.classes:includes("prop") then
+    if el.classes:includes("prop" or "rmdprop" or "rmdproposition") then
     return {
         pandoc.RawBlock("latex", table.concat({"\\begin{proposition}", "{", thmname, "}", "{", thmlabel, "}" }) ),
         el,
         pandoc.RawBlock("latex", "\\end{proposition}")
     }
     end
-
+    if el.classes:includes("defn" or "rmddefn" or "rmddefinition") then
+    return {
+        pandoc.RawBlock("latex", table.concat({"\\begin{definition}", "{", thmname, "}", "{", thmlabel, "}" })),
+        el,
+        pandoc.RawBlock("latex", "\\end{definition}")
+    }
+    end
+    if el.classes:includes("exam" or "rmdexam" or "rmdexample") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{example}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{example}")
+    }
+    end
+    if el.classes:includes("exer" or "rmdexer" or "rmdexercise") then
+        return {
+            pandoc.RawBlock("latex", "\\begin{exercise}"),
+            el,
+            pandoc.RawBlock("latex", "\\end{exercise}")
+        }
+    end
+    if el.classes:includes("sol" or "rmdsol" or "rmdsolution") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{solution}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{solution}")
+    }
+    end
+    if el.classes:includes("rmdthink") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{rmdthink}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{rmdthink}")
+    }
+    end
+    if el.classes:includes("rmdnote") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{rmdnote}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{rmdnote}")
+    }
+    end
+    if el.classes:includes("rmdtip") then
+    return {
+        pandoc.RawBlock("latex", "\\begin{rmdtip}"),
+        el,
+        pandoc.RawBlock("latex", "\\end{rmdtip}")
+    }
+    end
     if el.classes:includes("twocols") then
     return {
         pandoc.RawBlock("latex", "\\begin{multicols}{2}"),
